3361b6c247bfc6875f67584f68efcec1035f41a0
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2026-03-08 03:41:24 +0500

    GroqChoice and GroqMessage models added.

---
+++ MyFirstChatUI/Models/Groq/GroqChoice.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace MyFirstChatUI.Models.Groq
+{
+	public class GroqChoice
+	{
+		[JsonProperty("index")]
+		public int Index { get; set; }
+
+		[JsonProperty("message")]
+		public GroqMessage Message { get; set; }
+	}
+}
---
+++ MyFirstChatUI/Models/Groq/GroqMessage.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace MyFirstChatUI.Models.Groq
+{
+	public class GroqMessage
+	{
+		[JsonProperty("role")]
+		public string Role { get; set; }
+
+		[JsonProperty("content")]
+		public string Content { get; set; }
+	}
+}
