7c8cd97e77b54fbe049d23b5fbf4c103f8852a18
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2026-03-10 07:01:47 +0500

    Request Helper Implemented.

---
+++ MyFirstChatUI/Helpers/Groq/RequestHelper.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Nodes;
+
+namespace MyFirstChatUI.Helpers.Groq
+{
+	public class RequestHelper
+	{
+		public static JsonArray DeepCloneMessages(JsonArray messages)
+		{
+			return JsonNode.Parse(messages.ToJsonString())!.AsArray();
+		}
+	}
+}
